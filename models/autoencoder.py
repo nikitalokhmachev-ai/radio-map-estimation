3faf243d3739bdb78e6034054b5258aa721a0fc6
n1kkqt <n1kkqt@gmail.com>
2023-02-18 23:51:02 -0600

    fix evaluate bug in vae

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -46,10 +46,10 @@ class Autoencoder(torch.nn.Module):
             for i, data in enumerate(test_dl):
                     t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
                     t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
-                    t_channel_pow = t_channel_pow.flatten(1).to(device)
-                    t_y_point_pred = self.forward(t_x_point)
-                    building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float64)
-                    loss = (torch.norm((1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), dim=1) ** 2 / torch.sum(building_mask == 0, axis=1)).detach().cpu().tolist()
+                    t_channel_pow = t_channel_pow.flatten(1).to(device).detach().cpu().numpy()
+                    t_y_point_pred = self.forward(t_x_point).detach().cpu().numpy()
+                    building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float64).detach().cpu().numpy()
+                    loss = (np.linalg.norm((1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), axis=1) ** 2 / np.sum(building_mask == 0, axis=1)).tolist()
                     losses += loss
             
                     print(f'{np.sqrt(np.mean(loss))}')
