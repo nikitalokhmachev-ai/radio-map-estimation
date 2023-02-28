f4bbcd7e9fc44204a05093ab3301eed0f4c450b2
WilliamLockeIV <williamlockeiv@gmail.com>
2023-02-28 16:51:18 -0600

    fix error in UNetAutoencoder_NoMask fit function

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -235,7 +235,7 @@ class UNetAutoencoder_NoMask(UNetAutoencoder):
             for i, data in enumerate(train_dl):
                 optimizer.zero_grad()
                 t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
-                t_x_point = t_x_point[:0].unsqueeze(1)
+                t_x_point = t_x_point[:,0].unsqueeze(1)
                 t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
                 t_channel_pow = t_channel_pow.flatten(1).to(device)
                 t_y_point_pred = self.forward(t_x_point).to(torch.float64)
