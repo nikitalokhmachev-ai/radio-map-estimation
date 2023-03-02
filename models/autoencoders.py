ec1247fe6f6cb5fe7f0b352361b0b7c0cc719832
WilliamLockeIV <williamlockeiv@gmail.com>
2023-03-02 17:12:19 -0600

    implement SparseBaseAutoencoder_AvgPool, all Sparse Autoencoders now decide whether to include input mask in convolutions by checking self.enc_in parameter

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -7,6 +7,7 @@ from .resnet_vae import Encoder as ResnetVariationalEncoder, Decoder as ResnetVa
 from .sparse_base import Encoder as SparseBaseEncoder, Decoder as SparseBaseDecoder
 from .sparse_unet import Encoder as SparseUNetEncoder, Decoder as SparseUNetDecoder
 from .sparse_base_maxpool import Encoder as SparseBaseEncoder_MaxPool, Decoder as SparseBaseDecoder_MaxPool
+from .sparse_base_avgpool import Encoder as SparseBaseEncoder_AvgPool, Decoder as SparseBaseDecoder_AvgPool
 from .sparse_unet_maxpool import Encoder as SparseUnetEncoder_MaxPool, Decoder as SparseUNetDecoder_MaxPool
 from .autoencoder import Autoencoder
 
@@ -140,8 +141,9 @@ class ResnetVariationalAutoencoder(VariationalAutoencoder):
 
 
 class SparseBaseAutoencoder(Autoencoder):
-    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+    def __init__(self, enc_in, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
         super().__init__()
+        self.enc_in = enc_in
 
         self.encoder = SparseBaseEncoder(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
         self.decoder = SparseBaseDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
@@ -160,7 +162,11 @@ class SparseBaseAutoencoder(Autoencoder):
                 t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
                 t_channel_pow = t_channel_pow.flatten(1).to(device)
                 mask = (t_x_point[:,1] != 0).to(torch.float32)
-                t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).to(torch.float64)
+                if self.enc_in==1:      # Do sparse convolutions only over measurement channel
+                    t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).to(torch.float64)
+                elif self.enc_in==2:    # Do sparse convolutions over measurement and mask channels
+                    t_y_point_pred = self.forward(t_x_point, mask.unsqueeze(1)).to(torch.float64)
+                else: raise ValueError('Model enc_in must be 1 to convolve over measurement channel or 2 to convolve over measurement and mask channels.')
                 loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
                 if loss == 'rmse':
                     loss_ = torch.sqrt(loss_)
@@ -179,7 +185,11 @@ class SparseBaseAutoencoder(Autoencoder):
                     t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
                     t_channel_pow = t_channel_pow.flatten(1).to(device).detach().cpu().numpy()
                     mask = (t_x_point[:,1] != 0).to(torch.float32)
-                    t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).detach().cpu().numpy()
+                    if self.enc_in==1:      # Do sparse convolutions only over measurement channel
+                        t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).detach().cpu().numpy()
+                    elif self.enc_in==2:    # Do sparse convolutions over measurement and mask channels
+                        t_y_point_pred = self.forward(t_x_point, mask.unsqueeze(1)).detach().cpu().numpy()
+                    else: raise ValueError('Model enc_in must be 1 to convolve over measurement channel or 2 to convolve over measurement and mask channels.')
                     building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float64).detach().cpu().numpy()
                     loss = (np.linalg.norm((1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), axis=1) ** 2 / np.sum(building_mask == 0, axis=1)).tolist()
                     losses += loss
@@ -190,9 +200,27 @@ class SparseBaseAutoencoder(Autoencoder):
         
 
 
+class SparseBaseAutoencoder_MaxPool(SparseBaseAutoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__(enc_in=enc_in)
+
+        self.encoder = SparseBaseEncoder_MaxPool(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+        self.decoder = SparseBaseDecoder_MaxPool(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+
+
+
+class SparseBaseAutoencoder_AvgPool(SparseBaseAutoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__(enc_in=enc_in)
+
+        self.encoder = SparseBaseEncoder_AvgPool(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+        self.decoder = SparseBaseDecoder_AvgPool(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+
+
+
 class SparseUNetAutoencoder(SparseBaseAutoencoder):
     def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
-        super().__init__()
+        super().__init__(enc_in=enc_in)
 
         self.encoder = SparseUNetEncoder(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
         self.decoder = SparseUNetDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
@@ -201,21 +229,12 @@ class SparseUNetAutoencoder(SparseBaseAutoencoder):
         x, skip1, skip2, skip3 = self.encoder(x, mask)
         x = self.decoder(x, skip1, skip2, skip3)
         return x
-    
-
-
-class SparseBaseAutoencoder_MaxPool(SparseBaseAutoencoder):
-    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
-        super().__init__()
-
-        self.encoder = SparseBaseEncoder_MaxPool(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
-        self.decoder = SparseBaseDecoder_MaxPool(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
 
 
 
 class SparseUNetAutoencoder_MaxPool(SparseUNetAutoencoder):
     def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
-        super().__init__()
+        super().__init__(enc_in=enc_in)
 
         self.encoder = SparseUnetEncoder_MaxPool(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
         self.decoder = SparseUNetDecoder_MaxPool(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
