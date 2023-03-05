793cd10b669d83b711679e1dc5d0994c933219b4
n1kkqt <n1kkqt@gmail.com>
2023-03-04 20:16:41 -0600

    commit before pull

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -12,55 +12,6 @@ import numpy as np
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 
-class SparseBaseAutoencoder(Autoencoder):
-    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
-        super().__init__()
-
-        self.encoder = SparseBaseEncoder(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
-        self.decoder = SparseBaseDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
-
-    def forward(self, x, mask):
-        x = self.encoder(x, mask)
-        x = self.decoder(x)
-        return x
-
-    def fit(self, train_dl, optimizer, epochs=100, loss='mse'):
-        for epoch in range(epochs):
-            running_loss = 0.0
-            for i, data in enumerate(train_dl):
-                optimizer.zero_grad()
-                t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
-                t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
-                t_channel_pow = t_channel_pow.flatten(1).to(device)
-                mask = (t_x_point[:,1] != 0).to(torch.float32)
-                t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).to(torch.float64)
-                loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
-                if loss == 'rmse':
-                    loss_ = torch.sqrt(loss_)
-                loss_.backward()
-                optimizer.step()
-
-                running_loss += loss_.item()        
-                print(f'{loss_}, [{epoch + 1}, {i + 1:5d}] loss: {running_loss/(i+1)}')
-
-
-    def evaluate(self, test_dl, scaler):
-        losses = []
-        with torch.no_grad():
-            for i, data in enumerate(test_dl):
-                    t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
-                    t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
-                    t_channel_pow = t_channel_pow.flatten(1).to(device).detach().cpu().numpy()
-                    mask = (t_x_point[:,1] != 0).to(torch.float32)
-                    t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).detach().cpu().numpy()
-                    building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float64).detach().cpu().numpy()
-                    loss = (np.linalg.norm((1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), axis=1) ** 2 / np.sum(building_mask == 0, axis=1)).tolist()
-                    losses += loss
-            
-                    print(f'{np.sqrt(np.mean(loss))}')
-                    
-            return torch.sqrt(torch.Tensor(losses).mean())
-
 
 class BaseAutoencoder(Autoencoder):
     def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
@@ -184,3 +135,51 @@ class ResnetVariationalAutoencoder(VariationalAutoencoder):
         self.decoder = ResnetVariationalDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
         
 
+class SparseBaseAutoencoder(Autoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__()
+
+        self.encoder = SparseBaseEncoder(enc_in, enc_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+        self.decoder = SparseBaseDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+
+    def forward(self, x, mask):
+        x = self.encoder(x, mask)
+        x = self.decoder(x)
+        return x
+
+    def fit(self, train_dl, optimizer, epochs=100, loss='mse'):
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, data in enumerate(train_dl):
+                optimizer.zero_grad()
+                t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
+                t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
+                t_channel_pow = t_channel_pow.flatten(1).to(device)
+                mask = (t_x_point[:,1] != 0).to(torch.float32)
+                t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).to(torch.float64)
+                loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
+                if loss == 'rmse':
+                    loss_ = torch.sqrt(loss_)
+                loss_.backward()
+                optimizer.step()
+
+                running_loss += loss_.item()        
+                print(f'{loss_}, [{epoch + 1}, {i + 1:5d}] loss: {running_loss/(i+1)}')
+
+
+    def evaluate(self, test_dl, scaler):
+        losses = []
+        with torch.no_grad():
+            for i, data in enumerate(test_dl):
+                    t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
+                    t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
+                    t_channel_pow = t_channel_pow.flatten(1).to(device).detach().cpu().numpy()
+                    mask = (t_x_point[:,1] != 0).to(torch.float32)
+                    t_y_point_pred = self.forward(t_x_point[:,0].unsqueeze(1), mask.unsqueeze(1)).detach().cpu().numpy()
+                    building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float64).detach().cpu().numpy()
+                    loss = (np.linalg.norm((1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), axis=1) ** 2 / np.sum(building_mask == 0, axis=1)).tolist()
+                    losses += loss
+            
+                    print(f'{np.sqrt(np.mean(loss))}')
+                    
+            return torch.sqrt(torch.Tensor(losses).mean())
