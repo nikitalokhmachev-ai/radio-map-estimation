216914f3ff8956ec920d414f6ebee2e6c340f3c5
WilliamLockeIV <williamlockeiv@gmail.com>
2023-05-30 10:36:49 -0500

    Add RadioMapDataset class and sample_data function for RadioMapSeer dataset

--- data_utils.py
+++ data_utils.py
@@ -1,12 +1,17 @@
 import torch
 import torchvision
+from torchvision.io import read_image
 import torchvision.transforms as transforms
 import torch.nn.functional as F
 from torch import nn
+import os
+import json
 import numpy as np
 from torch.utils.data import Dataset
 from PIL import Image
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
+# Iterable Dataset class for Deep Completion Autoencoders data
 
 class MapDataset(torch.utils.data.IterableDataset):
     def __init__(self, pickles, scaler=None, building_value=None, unsampled_value=None, sampled_value=None):
@@ -93,3 +98,97 @@ def train_scaler(scaler, pickles):
     gen = file_path_generator(pickles, scaler=None)
     for t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, i in gen:
         scaler.fit(t_channel_pow)
+
+
+# Dataset class for RadioMapSeer data
+
+class RadioMapDataset(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.map_paths = sorted(glob.glob(os.path.join(data_dir, 'gain', 'DPM', '*')))
+
+    def __len__(self):
+        return len(self.map_paths)
+
+    def __getitem__(self, idx):
+        # Get path to complete map file
+        map_path = self.map_paths[idx]
+        map_name = os.path.basename(map_path)
+
+        # Get paths to corresponding building mask and tx location files
+        building_name = map_name.split('_')[0]
+        building_path = os.path.join(self.data_dir, 'png', 'buildings_complete', f'{building_name}.png')
+        tx_path = os.path.join(self.data_dir, 'antenna', f'{building_name}.json')
+        tx_name = int(map_name.split('_')[1][:-4])
+
+        # Open complete map, building mask, and tx loc files. 
+        complete_map = read_image(self.map_paths[idx])[0]
+        building_mask = read_image(building_path)[0]
+        with open(tx_path) as f:
+            tx_file = json.load(f)
+        x_loc, y_loc = tx_file[tx_name]
+
+        # Scale map and mask from 0 - 255 to 0 - 1. Scale tx_file by size of image.
+        complete_map = complete_map / 255
+        building_mask = building_mask / 255
+        x_loc = x_loc / complete_map.shape[1]
+        y_loc = y_loc / complete_map.shape[0]
+        tx_loc = torch.tensor([x_loc, y_loc])
+
+        # Sample map for model input
+        sampled_map, environment_mask = sample_map(sampling_factor=0.1, map_to_sample=complete_map, building_mask=building_mask)
+
+        # from MapDataset class, corresponds to:
+        # return t_x_points, t_y_points, t_y_mask, t_channel_pows, filename, i
+        # `complete_map` is returned twice for compatibility with previous class. `i` is replaced wtih tx_loc
+        return sampled_map, complete_map, environment_mask, complete_map[0],  map_path, tx_loc
+    
+
+def sample_map(sampling_factor, map_to_sample, building_mask):
+    """
+    This function is copied with some edits from the repository fachu000/deep-autoencoders-cartography 
+    https://github.com/fachu000/deep-autoencoders-cartography
+
+    Params:
+    sampling_factor: can be
+        - fraction between 0 and 1 determining on average the percentage of entries to select
+            as samples, i.e  sampling_factor= 0.3 will allow selection of 30 % of total entries of 
+            the map that are different from np.nan. 
+        - Interval (list or tuple of length 2). The aforementioned fraction is drawn uniformly at random 
+            within the interval [sampling_factor[0], sampling_factor[1] ] each time a map is sampled.
+    map_to_sample: 1 x Nx x Ny tensor, complete map of radio power, scaled between 0 and 1
+    building_mask: 1 x Nx x Ny tensor, binary mask of building locations, each entry is 1 if occupied by building, 0 otherwise
+
+    Returns:
+    sampled map: 1 x Nx x Ny tensor
+    environment_mask`: 1 x Nx x Ny, each entry is 1 if that grid point is sampled, -1 if occupied by building, 0 otherwise.
+    """
+    if np.size(sampling_factor) == 1:
+        pass
+    elif np.size(sampling_factor) == 2:
+        sampling_factor = np.round_((sampling_factor[1] - sampling_factor[0]) * np.random.rand() +
+                                    sampling_factor[0], decimals=2)
+    else:
+        Exception("invalid value of v_sampling_factor")
+    if sampling_factor == 1:
+        sampled_map = map_to_sample.clone()
+        environment_mask = torch.ones(map_to_sample.shape) -2 * building_mask
+    else:
+        flat_map = map_to_sample.clone().flatten()
+        flat_buildings = building_mask.clone().flatten()
+        flat_environment = torch.ones(flat_map.shape)
+        
+        building_idx = torch.nonzero(flat_buildings != 0).squeeze()
+        free_space_idx = torch.nonzero(flat_buildings == 0).squeeze()
+        unsampled_idx = np.random.choice(free_space_idx,
+                                        size=int((1 - sampling_factor) * len(free_space_idx)),
+                                        replace=False)
+        all_unsampled_idx = list(map(int, np.concatenate((building_idx, unsampled_idx),
+                                                        axis=0)))
+
+        flat_map[all_unsampled_idx] = 0
+        flat_environment[building_idx] = -1
+        flat_environment[unsampled_idx] = 0
+        sampled_map = torch.reshape(flat_map, map_to_sample.shape)
+        environment_mask = torch.reshape(flat_environment, map_to_sample.shape)
+    return sampled_map, environment_mask
