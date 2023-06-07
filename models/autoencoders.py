82afed32fcf8a4b206bf249b7038b05b2af576d1
WilliamLockeIV <williamlockeiv@gmail.com>
2023-06-07 13:43:44 -0500

    implement fit_wandb for TLPResUNetAutoencoder

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -204,37 +204,78 @@ class TLPResUNetAutoencoder(Autoencoder):
         x = self.decoder(x)
         return x, tx_loc
     
-    def fit(self, train_dl, optimizer, w_rec=0.5, w_loc=0.5, epochs=100, save_model_epochs = 25, save_model_dir = '/content', loss='mse'):
+    def step(self, batch, optimizer, w_rec=0.5, w_loc=0.5, train=True):
+        with torch.set_grad_enabled(train):
+            t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, tx_loc = batch
+            t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(torch.float32).to(device), t_y_mask.flatten(1).to(torch.float32).to(device)
+            t_channel_pow = t_channel_pow.flatten(1).to(torch.float32).to(device)
+            t_y_point_pred, tx_loc_pred = self.forward(t_x_point).to(torch.float32)
+            rec_loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
+            loc_loss_ = torch.nn.functional.mse_loss(tx_loc, tx_loc_pred).to(torch.float32)
+            loss_ = w_rec * rec_loss_ + w_loc * loc_loss_
+            if train:
+                loss_.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+        return loss_, rec_loss_, loc_loss_
+            
+    
+    def fit(self, train_dl, optimizer, w_rec=0.5, w_loc=0.5, epochs=100, save_model_epochs=25, save_model_dir ='/content'):
         for epoch in range(epochs):
             running_loss = 0.0
             rec_running_loss = 0.0
             loc_running_loss = 0.0
-            for i, data in enumerate(train_dl):
-                optimizer.zero_grad()
-                t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, tx_loc = data
-                t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(torch.float32).to(device), t_y_mask.flatten(1).to(torch.float32).to(device)
-                t_channel_pow = t_channel_pow.flatten(1).to(torch.float32).to(device)
-                t_y_point_pred, tx_loc_pred = self.forward(t_x_point).to(torch.float32)
-                rec_loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
-                loc_loss_ = torch.nn.functional.mse_loss(tx_loc, tx_loc_pred).to(torch.float32)
-                loss_ = w_rec * rec_loss_ + w_loc * loc_loss_
-                if loss == 'rmse':
-                    loss_ = torch.sqrt(loss_)
-                loss_.backward()
-                optimizer.step()
-
-                running_loss += loss_.item()   
-                rec_running_loss += rec_loss_.item()
-                loc_running_loss += loc_loss_.item()     
+            for i, batch in enumerate(train_dl):
+                loss_, rec_loss_, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=True)
+                running_loss += loss_.detach().item()
+                rec_running_loss += rec_loss_.detach().item()
+                loc_running_loss += loc_loss.detach().item()
                 print(f'{loss_}, [{epoch + 1}, {i + 1:5d}] loss: {running_loss/(i+1)}, reconstruction_loss: {rec_running_loss/(i+1)}, location_loss: {loc_running_loss/(i+1)}')
 
             if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
                 if not os.path.exists(save_model_dir):
                     os.makedirs(save_model_dir)
                 filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
-                torch.save(self, filepath)
+                self.save_model(filepath)
 
         return running_loss / (i+1)
+    
+    def fit_wandb(self, train_dl, test_dl, scaler, optimizer, project_name, run_name, w_rec=0.5, w_loc=0.5, epochs=100, save_model_epochs=25, save_model_dir='/content', loss='mse'):
+        import wandb
+        wandb.init(project=project_name, name=run_name)
+        for epoch in range(epochs):
+            running_loss, rec_running_loss, loc_running_loss = 0.0, 0.0, 0.0
+            for i, batch in enumerate(train_dl):
+                loss, rec_loss, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=True)
+                running_loss += loss.detach().item()
+                rec_running_loss += rec_loss.detach().item()
+                loc_running_loss += loc_loss.detach().item()
+                train_loss = running_loss/(i+1)
+                train_rec_loss = rec_running_loss/(i+1)
+                train_loc_loss = loc_running_loss/(i+1)
+                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {train_loss}, reconstruction_loss: {train_rec_loss}, location_loss: {train_loc_loss}')
+            
+            wandb.log({'train_loss': train_loss, 'train_reconstruction_loss': train_rec_loss, 'train_location_loss':train_loc_loss})
+        
+            if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
+                if not os.path.exists(save_model_dir):
+                    os.makedirs(save_model_dir)
+                filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
+                self.save_model(filepath)
+
+            running_loss, rec_running_loss, loc_running_loss = 0.0, 0.0, 0.0
+            for i, batch in enumerate(test_dl):
+                loss, rec_loss, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=False)
+                running_loss += loss.detach().item()
+                rec_running_loss += rec_loss.detach().item()
+                loc_running_loss += loc_loss.detach().item()
+                test_loss = running_loss/(i+1)
+                test_rec_loss = rec_running_loss/(i+1)
+                test_loc_loss = loc_running_loss/(i+1)
+                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {test_loss}, reconstruction_loss: {test_rec_loss}, location_loss: {test_loc_loss}')
+                
+                wandb.log({'test_loss': test_loss, 'test_reconstruction_loss': test_rec_loss, 'test_location_loss':test_loc_loss})
 
 
 # Variational Autoencoders
