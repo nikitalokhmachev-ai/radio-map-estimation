b00f284dd3f6d8ebfe6c1dbea8f7fb5260cba2db
WilliamLockeIV <williamlockeiv@gmail.com>
2023-06-07 21:37:06 -0500

    Renamed TLPResUNetAutoencoder to TLPResUNetAutoencoderXY_V1, implemented TLPAutoencoder base class and descendent classes TLPResUNetAutoencoderBCE_V1 and _V2 and TLPResUNetAutoencoderSoftmax_V1

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -3,7 +3,10 @@ from .resnet import Encoder as ResnetEncoder, Decoder as ResnetDecoder
 from .unet import Encoder as UNetEncoder, Decoder as UNetDecoder
 from .res_unet import Encoder as ResUNetEncoder, Decoder as ResUNetDecoder
 from .res_unet_concat import Encoder as ResUNetConcatEncoder, Decoder as ResUNetConcatDecoder
-from .tlp_resunet import Encoder as TLPResUNetEncoder, Decoder as TLPResUNetDecoder
+from .tlp_resunet_xy_v1 import Encoder as TLPResUNetEncoderXY_V1, Decoder as TLPResUNetDecoderXY_V1
+from .tlp_resunet_bce_v1 import Encoder as TLPResUNetEncoderBCE_V1, Decoder as TLPResUNetDecoderBCE_V1
+from .tlp_resunet_bce_v2 import Encoder as TLPResUNetEncoderBCE_V2, Decoder as TLPResUNetDecoderBCE_V2
+from .tlp_resunet_softmax_v1 import Encoder as TLPResUNetEncoderSoftmax_V1, Decoder as TLPResUNetDecoderSoftmax_V1
 
 from .vae import Encoder as VariationalEncoder, Decoder as VariationalDecoder
 from .resnet_vae import Encoder as ResnetVariationalEncoder, Decoder as ResnetVariationalDecoder
@@ -41,6 +44,7 @@ from .unet_concat_map_mask import Encoder as UNetConcatMapMaskEncoder, Decoder a
 from .unet_concat_input import Encoder as UNetConcatInputEncoder, Decoder as UNetConcatInputDecoder
 from .unet_dual_encoder import Encoder as UNetDualEncoder
 from .autoencoder import Autoencoder
+from .tlp_autoencoder import TLPAutoencoder
 
 import os
 import torch
@@ -193,95 +197,52 @@ class UNetAutoencoder_SeparateMasks(UNetAutoencoder):
             return torch.sqrt(torch.Tensor(losses).mean())
 
 
-class TLPResUNetAutoencoder(Autoencoder):
+class TLPResUNetAutoencoderXY_V1(TLPAutoencoder):
     def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, xy_features=8, leaky_relu_alpha=0.3):
         super().__init__()
-        self.encoder = TLPResUNetEncoder(enc_in, enc_out, n_dim, xy_features, leaky_relu_alpha=leaky_relu_alpha)
-        self.decoder = TLPResUNetDecoder(enc_out, dec_out, n_dim, leaky_relu_alpha=leaky_relu_alpha)
+        self.encoder = TLPResUNetEncoderXY_V1(enc_in, enc_out, n_dim, xy_features, leaky_relu_alpha)
+        self.decoder = TLPResUNetDecoderXY_V1(enc_out, dec_out, n_dim, leaky_relu_alpha)
 
     def forward(self, x):
-        x, tx_loc = self.encoder(x)
-        x = self.decoder(x)
+        x, tx_loc, skip1, skip2, skip3 = self.encoder(x)
+        x = self.decoder(x, skip1, skip2, skip3)
         return x, tx_loc
     
-    def step(self, batch, optimizer, w_rec=0.5, w_loc=0.5, train=True):
-        with torch.set_grad_enabled(train):
-            t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, tx_loc = batch
-            t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(torch.float32).to(device), t_y_mask.flatten(1).to(torch.float32).to(device)
-            t_channel_pow = t_channel_pow.flatten(1).to(torch.float32).to(device)
-            tx_loc = tx_loc.to(torch.float32).to(device)
-            t_y_point_pred, tx_loc_pred = self.forward(t_x_point)
-            t_y_point_pred = t_y_point_pred.to(torch.float32)
-            tx_loc_pred = tx_loc_pred.to(torch.float32)
-            rec_loss_ = torch.nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
-            loc_loss_ = torch.nn.functional.mse_loss(tx_loc, tx_loc_pred).to(torch.float32)
-            loss_ = w_rec * rec_loss_ + w_loc * loc_loss_
-            if train:
-                loss_.backward()
-                optimizer.step()
-                optimizer.zero_grad()
 
-        return loss_, rec_loss_, loc_loss_
-            
+class TLPResUNetAutoencoderBCE_V1(TLPAutoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__()
+        self.encoder = TLPResUNetEncoderBCE_V1(enc_in, enc_out, n_dim, leaky_relu_alpha)
+        self.decoder = TLPResUNetDecoderBCE_V1(enc_out, dec_out, n_dim, leaky_relu_alpha)
+
+    def forward(self, x):
+        x, skip1, skip2, skip3 = self.encoder(x)
+        x, tx_loc = self.decoder(x, skip1, skip2, skip3)
+        return x, tx_loc
+
+
+class TLPResUNetAutoencoderBCE_V2(TLPAutoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__()
+        self.encoder = TLPResUNetEncoderBCE_V2(enc_in, enc_out, n_dim, leaky_relu_alpha)
+        self.decoder = TLPResUNetDecoderBCE_V2(enc_out, dec_out, n_dim, leaky_relu_alpha)
+
+    def forward(self, x):
+        x, skip1, skip2, skip3 = self.encoder(x)
+        x, tx_loc = self.decoder(x, skip1, skip2, skip3)
+        return x, tx_loc
     
-    def fit(self, train_dl, optimizer, w_rec=0.5, w_loc=0.5, epochs=100, save_model_epochs=25, save_model_dir ='/content'):
-        for epoch in range(epochs):
-            running_loss = 0.0
-            rec_running_loss = 0.0
-            loc_running_loss = 0.0
-            for i, batch in enumerate(train_dl):
-                loss_, rec_loss_, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=True)
-                running_loss += loss_.detach().item()
-                rec_running_loss += rec_loss_.detach().item()
-                loc_running_loss += loc_loss.detach().item()
-                print(f'{loss_}, [{epoch + 1}, {i + 1:5d}] loss: {running_loss/(i+1)}, reconstruction_loss: {rec_running_loss/(i+1)}, location_loss: {loc_running_loss/(i+1)}')
 
-            if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
-                if not os.path.exists(save_model_dir):
-                    os.makedirs(save_model_dir)
-                filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
-                self.save_model(filepath)
+class TLPResUNetAutoencoderSoftmax_V1(TLPAutoencoder):
+    def __init__(self, enc_in=2, enc_out=4, dec_out=1, n_dim=27, leaky_relu_alpha=0.3):
+        super().__init__()
+        self.encoder = TLPResUNetEncoderSoftmax_V1(enc_in, enc_out, n_dim, leaky_relu_alpha)
+        self.decoder = TLPResUNetDecoderSoftmax_V1(enc_out, dec_out, n_dim, leaky_relu_alpha)
 
-        return running_loss / (i+1)
-    
-    def fit_wandb(self, train_dl, test_dl, optimizer, project_name, run_name, w_rec=0.5, w_loc=0.5, epochs=100, save_model_epochs=25, save_model_dir='/content'):
-        import wandb
-        wandb.init(project=project_name, name=run_name)
-        for epoch in range(epochs):
-            running_loss, rec_running_loss, loc_running_loss = 0.0, 0.0, 0.0
-            for i, batch in enumerate(train_dl):
-                loss, rec_loss, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=True)
-                running_loss += loss.detach().item()
-                rec_running_loss += rec_loss.detach().item()
-                loc_running_loss += loc_loss.detach().item()
-                train_loss = running_loss/(i+1)
-                train_rec_loss = rec_running_loss/(i+1)
-                train_loc_loss = loc_running_loss/(i+1)
-                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {train_loss}, reconstruction_loss: {train_rec_loss}, location_loss: {train_loc_loss}')
-            
-            wandb.log({'train_loss': train_loss, 'train_reconstruction_loss': train_rec_loss, 'train_location_loss':train_loc_loss})
-        
-            if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
-                if not os.path.exists(save_model_dir):
-                    os.makedirs(save_model_dir)
-                filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
-                self.save_model(filepath)
-
-            running_loss, rec_running_loss, loc_running_loss = 0.0, 0.0, 0.0
-            for i, batch in enumerate(test_dl):
-                loss, rec_loss, loc_loss = self.step(batch, optimizer, w_rec, w_loc, train=False)
-                running_loss += loss.detach().item()
-                rec_running_loss += rec_loss.detach().item()
-                loc_running_loss += loc_loss.detach().item()
-                test_loss = running_loss/(i+1)
-                test_rec_loss = rec_running_loss/(i+1)
-                test_loc_loss = loc_running_loss/(i+1)
-                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {test_loss}, reconstruction_loss: {test_rec_loss}, location_loss: {test_loc_loss}')
-                
-            wandb.log({'test_loss': test_loss, 'test_reconstruction_loss': test_rec_loss, 'test_location_loss':test_loc_loss})
-
-
-
+    def forward(self, x):
+        x, skip1, skip2, skip3 = self.encoder(x)
+        x, tx_loc = self.decoder(x, skip1, skip2, skip3)
+        return x, tx_loc
 
 
 # Variational Autoencoders
