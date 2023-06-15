e35570530cab33a43c6d864b61db445a6a9e8733
WilliamLockeIV <williamlockeiv@gmail.com>
2023-06-15 16:42:08 -0500

    implement UNet_V2 under original_autoencoder

--- original_autoencoder/unet.py
+++ original_autoencoder/unet.py
@@ -197,3 +197,102 @@ class UNet(nn.Module):
 
     def save_model(self, out_path):
         torch.save(self, out_path)
+
+
+
+class UNet_V2(UNet):
+    '''
+    This UNet is the most like the PIMRC model. It only changes Upsampling to ConvTranspose2d, LeakyRelu to Relu, adds
+    BatchNorm in the Convolution Blocks, and adds a single 1x1 Convolution on the last layer to get the right output shape.  
+    This is to check  that we can actually get the same performance out of a model if using basically the same architecture 
+    (i.e. that the changes listed above, or some smaller change we missed, don't significantly impact performance).
+
+    We also change the UNet._block so that it includes 3 Convolutions (like the PIMRC UNet) and includes a Bias term.
+    '''
+
+    def __init__(self, in_channels=2, latent_channels=4, out_channels=1, features=32):
+        super(UNet, self).__init__()
+
+        self.encoder1 = UNet._block(in_channels, features, name="enc1")
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder2 = UNet._block(features, features, name="enc2")
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder3 = UNet._block(features, features, name="enc3")
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.encoder4 = nn.Conv2d(features, latent_channels, name="enc4")
+        # encoder4 (a single Convolution Layer) takes the place of "bottleneck" but serves same purpose
+
+        # convolve once (with a single Convolution Layer) before upsampling / deconvoluting
+        self.decoder4 = nn.Conv2d(latent_channels, features, name="dec4")
+        self.upconv3 = nn.ConvTranspose2d(features, features, kernel_size=2, stride=2)
+        self.decoder3 = UNet._block((features) * 2, features, name="dec3")
+        self.upconv2 = nn.ConvTranspose2d(features, features, kernel_size=2, stride=2)
+        self.decoder2 = UNet._block((features) * 2, features, name="dec2")
+        self.upconv1 = nn.ConvTranspose2d(features, features, kernel_size=2, stride=2)
+        self.decoder1 = UNet._block(features * 2, features, name="dec1")
+
+        # Here is one extra 1x1 Convolution to change the output into the right shape
+        self.conv = nn.Conv2d(in_channels=features, out_channels=out_channels, kernel_size=1)
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.pool1(enc1))
+        enc3 = self.encoder3(self.pool2(enc2))
+        enc4 = self.encoder4(self.pool3(enc3))
+
+        dec4 = self.decoder4(enc4)
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+        return torch.sigmoid(self.conv(dec1))
+
+    @staticmethod
+    def _block(in_channels, features, name):
+        return nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        name + "conv1",
+                        nn.Conv2d(
+                            in_channels=in_channels,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu1", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv2",
+                        nn.Conv2d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu2", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv3",
+                        nn.Conv2d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm3", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu3", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
