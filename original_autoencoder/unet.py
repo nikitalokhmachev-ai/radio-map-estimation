8871f8021a5fb0ef173ab526995fd7c2cb29d724
WilliamLockeIV <williamlockeiv@gmail.com>
2023-06-16 11:28:22 -0500

    fix errors in UNet_V2

--- original_autoencoder/unet.py
+++ original_autoencoder/unet.py
@@ -200,7 +200,7 @@ class UNet(nn.Module):
 
 
 
-class UNet_V2(UNet):
+class UNet_V2(nn.Module):
     '''
     This UNet is the most like the PIMRC model. It changes Upsampling to ConvTranspose2d, LeakyRelu to Relu (or Sigmoid), 
     in the last layer), adds BatchNorm in the Convolution Blocks, adds a single 1x1 Convolution on the last layer to get
@@ -217,8 +217,11 @@ class UNet_V2(UNet):
     '''
 
     def __init__(self, in_channels=2, latent_channels=4, out_channels=1, features=27):
-        # TODO: Figure out the best way to call super().__init__() here. Since we don't strictly need to initialize UNet,
-        # I think we could probably call super(UNet, self).__init__() to just initialize UNet's parent, i.e. nn.Module
+        # TODO: Figure out the best way to call super().__init__() here. 
+        '''Right now I've copied all code from UNet, so I don't need to inherit from it. If I inherit from UNet, I can call
+        super(UNet_V2, self).__init__(), but that initializes UNet and adds a bunch of parameters I don't need/use. I can
+        also call super(UNet, self).__init__(), which seems to just initialize UNet's parent, i.e. nn.Module, which is what
+        I want, but I'm nervous there might be some other effects I'm missing there.'''
         super().__init__()
 
         self.encoder1 = UNet_V2._block(in_channels, features, name="enc1")
@@ -305,3 +308,100 @@ class UNet_V2(UNet):
             )
         )
     
+    def step(self, batch, optimizer, train=True):
+        with torch.set_grad_enabled(train):
+            t_x_point, t_y_point, t_y_mask, _, _, tx_loc = batch
+            t_x_point, t_y_point = t_x_point.to(torch.float32).to(device), t_y_point.to(torch.float32).to(device)
+            t_y_mask, tx_loc = t_y_mask.to(torch.float32).to(device), tx_loc.to(torch.float32).to(device)
+
+            t_y_point_pred = self.forward(t_x_point).to(torch.float32)
+
+            loss_ = nn.functional.mse_loss(t_y_point * t_y_mask, t_y_point_pred * t_y_mask).to(torch.float32)
+            
+            if train:
+                loss_.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
+        return loss_
+    
+    def fit(self, train_dl, optimizer, epochs=100, save_model_epochs=25, save_model_dir ='/content'):
+        for epoch in range(epochs):
+            running_loss = 0.0
+            for i, batch in enumerate(train_dl):
+                loss_ = self.step(batch, optimizer, train=True)
+                running_loss += loss_.detach().item()
+                print(f'{loss_}, [{epoch + 1}, {i + 1:5d}] loss: {running_loss/(i+1)}')
+
+            if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
+                if not os.path.exists(save_model_dir):
+                    os.makedirs(save_model_dir)
+                filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
+                self.save_model(filepath)
+
+        return running_loss / (i+1)
+    
+
+    def fit_wandb(self, train_dl, test_dl, optimizer, project_name, run_name, epochs=100, save_model_epochs=25, save_model_dir='/content'):
+        import wandb
+        wandb.init(project=project_name, name=run_name)
+        for epoch in range(epochs):
+            train_running_loss = 0.0
+            for i, batch in enumerate(train_dl):
+                loss = self.step(batch, optimizer, train=True)
+                train_running_loss += loss.detach().item()
+                train_loss = train_running_loss/(i+1)
+                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {train_loss}')
+                    
+            if (epoch + 1) % save_model_epochs == 0 or epoch == epochs - 1:
+                if not os.path.exists(save_model_dir):
+                    os.makedirs(save_model_dir)
+                filepath = os.path.join(save_model_dir, f'epoch_{epoch}.pth')
+                self.save_model(filepath)
+
+            test_running_loss = 0.0
+            for i, batch in enumerate(test_dl):
+                loss = self.step(batch, optimizer, train=False)
+                test_running_loss += loss.detach().item()
+                test_loss = test_running_loss/(i+1)
+                print(f'{loss}, [{epoch + 1}, {i + 1:5d}] loss: {test_loss}')
+                
+            wandb.log({'train_loss': train_loss, 'test_loss': test_loss})
+
+
+    def evaluate(self, test_dl, scaler, dB_max=-47.84, dB_min=-147, no_scale=False):
+        losses = []
+        with torch.no_grad():
+            for i, data in enumerate(test_dl):
+                    t_x_point, t_y_point, t_y_mask, t_channel_pow, file_path, j = data
+                    t_x_point, t_y_point, t_y_mask = t_x_point.to(torch.float32).to(device), t_y_point.flatten(1).to(device), t_y_mask.flatten(1).to(device)
+                    t_channel_pow = t_channel_pow.flatten(1).to(device).detach().cpu().numpy()
+                    t_y_point_pred = self.forward(t_x_point).flatten(1).detach().cpu().numpy()
+                    building_mask = (t_x_point[:,1,:,:].flatten(1) == -1).to(torch.float32).detach().cpu().numpy()
+                    if scaler:
+                        loss = (np.linalg.norm(
+                            (1 - building_mask) * (scaler.reverse_transform(t_channel_pow) - scaler.reverse_transform(t_y_point_pred)), axis=1) ** 2 
+                            / np.sum(building_mask == 0, axis=1)).tolist()
+                    else:
+                        if no_scale==True:
+                            loss = (np.linalg.norm(
+                                (1 - building_mask) * (t_channel_pow - t_y_point_pred), axis=1) ** 2 
+                                / np.sum(building_mask == 0, axis=1)).tolist()
+                        else:
+                            loss = (np.linalg.norm(
+                                (1 - building_mask) * (self.scale_to_dB(t_channel_pow, dB_max, dB_min) - self.scale_to_dB(t_y_point_pred, dB_max, dB_min)), axis=1) ** 2 
+                                / np.sum(building_mask == 0, axis=1)).tolist()
+                    losses += loss
+                    print(f'{np.sqrt(np.mean(loss))}')
+                    
+            return torch.sqrt(torch.Tensor(losses).mean())
+        
+
+    def scale_to_dB(self, value, dB_max, dB_min):
+        range_dB = dB_max - dB_min
+        dB = value * range_dB + dB_min
+        return dB
+    
+
+    def save_model(self, out_path):
+        torch.save(self, out_path)
