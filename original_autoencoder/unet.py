611a4fa233b559c72d53a598381ca60c7daaaba8
WilliamLockeIV <williamlockeiv@gmail.com>
2023-06-17 01:46:22 -0500

    implement UNet_V3

--- original_autoencoder/unet.py
+++ original_autoencoder/unet.py
@@ -24,26 +24,16 @@ class UNet(nn.Module):
 
         self.bottleneck = UNet._block(features * 8, features * 16, name="bottleneck")
 
-        self.upconv4 = nn.ConvTranspose2d(
-            features * 16, features * 8, kernel_size=2, stride=2
-        )
+        self.upconv4 = nn.ConvTranspose2d(features * 16, features * 8, kernel_size=2, stride=2)
         self.decoder4 = UNet._block((features * 8) * 2, features * 8, name="dec4")
-        self.upconv3 = nn.ConvTranspose2d(
-            features * 8, features * 4, kernel_size=2, stride=2
-        )
+        self.upconv3 = nn.ConvTranspose2d(features * 8, features * 4, kernel_size=2, stride=2)
         self.decoder3 = UNet._block((features * 4) * 2, features * 4, name="dec3")
-        self.upconv2 = nn.ConvTranspose2d(
-            features * 4, features * 2, kernel_size=2, stride=2
-        )
+        self.upconv2 = nn.ConvTranspose2d(features * 4, features * 2, kernel_size=2, stride=2)
         self.decoder2 = UNet._block((features * 2) * 2, features * 2, name="dec2")
-        self.upconv1 = nn.ConvTranspose2d(
-            features * 2, features, kernel_size=2, stride=2
-        )
+        self.upconv1 = nn.ConvTranspose2d(features * 2, features, kernel_size=2, stride=2)
         self.decoder1 = UNet._block(features * 2, features, name="dec1")
 
-        self.conv = nn.Conv2d(
-            in_channels=features, out_channels=out_channels, kernel_size=1
-        )
+        self.conv = nn.Conv2d(in_channels=features, out_channels=out_channels, kernel_size=1)
 
     def forward(self, x):
         enc1 = self.encoder1(x)
@@ -260,6 +250,99 @@ class UNet_V2(UNet):
         dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)
         return torch.sigmoid(self.conv(dec1))
+
+    @staticmethod
+    def _block(in_channels, features, name):
+        return nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        name + "conv1",
+                        nn.Conv2d(
+                            in_channels=in_channels,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm1", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu1", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv2",
+                        nn.Conv2d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm2", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu2", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv3",
+                        nn.Conv2d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=True,
+                        ),
+                    ),
+                    (name + "norm3", nn.BatchNorm2d(num_features=features)),
+                    (name + "relu3", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
+    
+
+class UNet_V3(UNet):
+    '''
+    '''
+
+    def __init__(self, in_channels=2, latent_channels=128, out_channels=1, features=[32,32,64]):
+        # TODO: Figure out the best way to call super().__init__() here. 
+        super(UNet, self).__init__()
+
+        # Use the same 3-layer blocks as UNet_V2
+        self.encoder1 = UNet_V2._block(in_channels, features[0], name="enc1")
+        self.pool1 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.encoder2 = UNet_V2._block(features[0], features[1], name="enc2")
+        self.pool2 = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.encoder3 = UNet_V2._block(features[1], features[2], name="enc3")
+        self.pool3 = nn.AvgPool2d(kernel_size=2, stride=2)
+
+        self.bottleneck = UNet_V2._block(features[2], latent_channels, name='bottleneck')
+
+        self.upconv3 = nn.ConvTranspose2d(latent_channels, features[2], kernel_size=2, stride=2)
+        self.decoder3 = UNet_V2._block(features[2] * 2, features[2], name="dec3")
+        self.upconv2 = nn.ConvTranspose2d(features[2], features[1], kernel_size=2, stride=2)
+        self.decoder2 = UNet_V2._block(features[1] * 2, features[1], name="dec2")
+        self.upconv1 = nn.ConvTranspose2d(features[1], features[0], kernel_size=2, stride=2)
+        self.decoder1 = UNet_V2._block(features[0] * 2, features[0], name="dec1")
+
+        # Here is one extra 1x1 Convolution to change the output into the right shape
+        self.conv = nn.Conv2d(in_channels=features[0], out_channels=out_channels, kernel_size=1)
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(self.pool1(enc1))
+        enc3 = self.encoder3(self.pool2(enc2))
+
+        bottleneck = self.bottleneck(self.pool(enc3))
+
+        dec3 = self.upconv3(bottleneck)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.decoder3(dec3)
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.decoder2(dec2)
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.decoder1(dec1)
+        map = torch.sigmoid(self.conv(dec1))
+        return map
 
     @staticmethod
     def _block(in_channels, features, name):
